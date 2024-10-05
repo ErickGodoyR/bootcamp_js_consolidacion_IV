1bbb34c484ace244ba334ccb2552ab1ae9e8c1b0
erick godoy <erickalexis23@hotmail.com>
2024-10-05 16:08:29 -0300

    se conecta api

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,64 @@
 //https://swapi.dev/api/people/
 
-$(document).ready(()=>{
-    const urlAPI = "https://swapi.dev/api/people/"
+const botonUno = document.getElementById("btnUno")
+const botonDos = document.getElementById("btnDos")
+const botonTres = document.getElementById("btnTres")
 
-    $.ajax({
-        url: urlAPI,
-        method: "GET",
-        dataType: "json",
-        success: {
 
-        },
-        error: {}
-    })
+async function mostrarInfo(){
+    const urlAPI = await fetch("https://swapi.dev/api/people/")
+    const data = await urlAPI.json()
 
-})
+    let resultado = data.results
+
+    const crearCard = ({name}) => {
+        return `
+            <div>
+                <h2>${name}</h2>
+            </div>
+        `
+    }
+
+    let elementos = document.getElementById('personajes')
+    let html = ""
+
+    for(let i=0; i<5; i++){
+        html += crearCard(resultado[i])
+        console.log(resultado[i])
+    }
+
+    elementos.innerHTML = html
+
+}
+
+
+botonUno.onclick = mostrarInfo
+
+
+
+async function mostrarMedianos() {
+    const urlAPIMedianos = await fetch("https://swapi.dev/api/people/")
+    const data = await urlAPIMedianos.json()
+
+    let resultadoMedianos = data.results
+
+    const crearCard = ({name}) => {
+        return `
+            <div>
+                <h2>${name}</h2>
+            </div>
+        `
+    }
+
+    let elementos = document.getElementById('personajes')
+    let html = ""
+
+    for(let i=5; i<10; i++){
+        html += crearCard(resultadoMedianos[i])
+        console.log(resultadoMedianos[i])
+    }
+
+    elementos.innerHTML = html
+}
+
+botonDos.onclick = mostrarMedianos
